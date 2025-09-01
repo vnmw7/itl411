97e0f8bbc4124565fd07b8cde45c090909e5c53b
Vincent Manalo <146041946+vnmw7@users.noreply.github.com>
2025-09-02 06:57:01 +0800

    handle missing data

--- read_csv.py
+++ read_csv.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
+
 df = pd.read_csv('yes2.csv')
 print(df.head())
+
+print(df.isnull().sum())
+mode = df['Address'].mode()[0]
+df['Address'].fillna(mode, inplace=True)
+df['Date of Birth'].fillna(method='ffill', inplace=True)
+
+
+
+
