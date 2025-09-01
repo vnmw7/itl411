4d3689de22be51000c14089ad913810b4fe4581e
Vincent Manalo <146041946+vnmw7@users.noreply.github.com>
2025-09-02 07:01:56 +0800

    drop duplicates

--- read_csv.py
+++ read_csv.py
@@ -4,11 +4,13 @@ import pandas as pd
 df = pd.read_csv('yes2.csv')
 print(df.head())
 
+
 print(df.isnull().sum())
 mode = df['Address'].mode()[0]
 df['Address'].fillna(mode, inplace=True)
 df['Date of Birth'].fillna(method='ffill', inplace=True)
 
 
+df.drop_duplicates(inplace=True)
 
-
+#remove outliers
